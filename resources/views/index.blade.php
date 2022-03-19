9e7922aa32d1874a62b83024a17a57cf2089181c
Olamide Olaniyan <olaniyanolamide42@hotmail.com>
2022-03-19 15:00:03 +0100

    Controller file added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,20 @@
 @extends('layouts.main')
 
 @section('content')
-    Nothing here
+    <div class="container-lg" style="margin: 0 auto;">
+        <div class="row mt-5">
+
+            <div class="col-lg-3 col-md-4 col-sm-12 text-center mb-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="" alt="">
+                    <div class="card-body">
+                        <div class="card-title">title</div>
+                        <div class="card-text">content</div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 @endsection
 
